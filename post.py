8a427ffdfffda5c3c735d8ab9b2f3f1452a2d6fe
sotojang <mrjang28@gmail.com>
2020-07-19 01:07:48 +0900

    Add files via upload

--- post.py
+++ post.py
@@ -7,11 +7,17 @@ headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
 for x in range(5):
     temperature = randint(0, 30)
+    humidity = randint(0, 30)
     # value = randint(0, 30)
     print("The temperature is %s celsius" % temperature)
+    print("The Humidity is %s celsius" % humidity)
     time.sleep(3)
 
     payload = {'sensor': str("temperature"), 'value': int(temperature) }
+    payloadHumidit = {'sensor': str("humidity"), 'value': int(humidity) }
+
+    payload.update(payloadHumidit)
+
 
     r = requests.post('http://localhost:3000/messages', data=json.dumps(payload), headers=headers)
 
